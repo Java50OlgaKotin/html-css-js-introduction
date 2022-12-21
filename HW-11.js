fb10376249947c9cd334370c83b307e4812782c6
Olga Kotin <o.b.baglay@gmail.com>
2022-12-21 14:23:50 +0200

    Home work 11. 4 tasks.

---
+++ HW-11.js
@@ -0,0 +1,89 @@
+
+let number = -762903.45;
+
+///// task 1 /////
+
+let res1 = getDigitsSum(number);
+console.log("Task 1. Sum of digits = " + res1);
+
+function getDigitsSum(number)
+{
+    if (number < 0)
+        number = -number;
+
+    number = Math.trunc(number);
+
+    let sum = 0;
+    let rem = 0;
+    do{
+        rem = number % 10;
+        sum = sum + rem;
+        number = number / 10;
+        number = Math.trunc(number);
+    } while(number != 0);
+
+    return sum;
+}
+
+///// task 2 /////
+
+let res2 = eval('9000/((10+20)**2)');
+console.log("Task 2. Result of expression: " + res2);
+
+///// task 3 /////
+
+let ananas = "A" + ("ab" / 2) + "AS";
+let res3 = ananas.toLocaleLowerCase();
+console.log("Task 3. " + res3);
+
+///// task 4 /////
+
+let res5 = digitsCount(number);
+function digitsCount(number)
+{
+    number = Math.trunc(number);
+    let dCount = 0;
+    while(number != 0)
+        {
+            number = number / 10;
+            number = Math.trunc(number);
+            dCount++;
+        }
+
+return (dCount - 1);
+}
+
+let res6 = multiplier(res5);
+function multiplier(dCount)
+{
+    let mult = 1;
+    while(dCount > 0)
+        {
+            mult = mult*10;
+            dCount--;
+        }
+return mult;
+}
+
+let res4 = reverseNumber(number);
+console.log("Task 4. Reverse number: " + res4);
+
+function reverseNumber(number)
+{
+    number = Math.trunc(number);
+    let newNum = 0;
+    let rem = 0;
+    let mult = res6;
+    
+do
+    {
+        rem = (number % 10) * mult;
+        mult = mult / 10;
+        newNum = newNum + rem;
+        number = number / 10;
+        number = Math.trunc(number);
+    }while(number != 0 && mult > 0);
+
+return newNum;
+}
+
---
+++ apps.js
@@ -0,0 +1,38 @@
+
+let num = 40.8;
+console.log(num);
+num = Math.floor(num);
+num = Math.trunc(num);
+num = Math.ceil(num);
+num = Math.round(num);
+num **= 2;
+
+let str = "abcd";
+let str1 = 'abcd';
+let res33 = "123" + 56;
+let res31 = "123" - 56;
+let res32 = Math.round("123" / 56);
+let res3 = "123" / "2";
+let res4 = "ab" / 8;
+
+let res5 = +"123" + 56;
+
+let res6 = true + true;
+let res7 = 123 && 256;
+let res8 = 200 && 0;
+
+let a = 33;
+let error = true;
+let res9 = a && 256 / a;
+let message = error && "some error";
+
+let res10 = a || 10;
+
+let res11 = "123" > "9";
+let res12 = "123" > 9;
+
+let res = "123" == 123; //true
+let res1 = "123" === 123; //faulse
+
+
+
